32f3f0846bd6d18f42012e6357fadcb0abd1c0c4
kordano <konrad.kuehne@rocketmail.com>
2014-08-19 11:31:11 +0200

    create backend

--- project.clj
+++ project.clj
@@ -1,6 +1,64 @@
 (defproject lesezeichen "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+
+  :description "A simple bookmark app"
+
   :url "http://example.com/FIXME"
+
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2268"]
+                 [org.clojure/core.cache "0.6.3"]
+                 [org.clojure/core.memoize "0.5.6" :exclusions [org.clojure/core.cache]]
+                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
+
+                 [log4j/log4j "1.2.17" :exclusions [javax.mail/mail javax.jms/jms com.sun.jdmk/jmxtools com.sun.jmx/jmxri]]
+                 [org.slf4j/slf4j-log4j12 "1.7.6"]
+                 [org.clojure/tools.logging "0.2.6"]
+
+                 [http-kit "2.1.18"]
+                 [ring "1.2.2"]
+                 [com.cemerick/friend "0.2.1"]
+                 [enlive "1.1.5"]
+                 [compojure "1.1.6"]
+
+                 [prismatic/dommy "0.1.2"]
+                 [datascript "0.1.6"]
+                 [om "0.6.3"]
+                 [kioo "0.4.0"]
+                 [figwheel "0.1.3-SNAPSHOT"]
+                 [com.facebook/react "0.9.0.1"]
+                 [net.polyc0l0r/geschichte "0.1.0-SNAPSHOT"]
+
+                 [weasel "0.2.1"]]
+
+  :source-paths ["src/cljs" "src/clj"]
+
+  :min-lein-version "2.0.0"
+
+  :main tresor.core
+
+  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+
+  :plugins [[lein-cljsbuild "1.0.3"]
+            [lein-figwheel "0.1.3-SNAPSHOT"]]
+
+  :figwheel {:http-server-root "public"
+             :port 3449
+             :css-dirs ["resources/public/css"]}
+
+  :cljsbuild
+  {:builds
+   [{:id "dev"
+     :source-paths ["src/cljs"]
+     :compiler {:output-to "resources/public/js/compiled/main.js"
+                :output-dir "resources/public/js/compiled/out"
+                :optimizations :none
+                :source-map true}}
+    {:id "prod"
+     :source-paths ["src/cljs"]
+     :compiler {:output-to "resources/public/js/main.js"
+                :optimizations :simple}}]}
+
+  )
--- src/clj/lesezeichen/core.clj
+++ src/clj/lesezeichen/core.clj
@@ -1,6 +1,130 @@
-(ns lesezeichen.core)
+(ns lesezeichen.core
+  (:gen-class :main true)
+  (:require [clojure.edn :as edn]
+            [net.cgrand.enlive-html :refer [deftemplate set-attr substitute html] :as enlive]
+            [clojure.java.io :as io]
+            [compojure.route :refer [resources]]
+            [compojure.core :refer [GET POST defroutes]]
+            [geschichte.repo :as repo]
+            [geschichte.stage :as s]
+            [geschichte.meta :refer [update]]
+            [geschichte.sync :refer [server-peer client-peer]]
+            [geschichte.platform :refer [create-http-kit-handler!]]
+            [geschichte.auth :refer [auth]]
+            [konserve.store :refer [new-mem-store]]
+            [konserve.platform :refer [new-couch-store]]
+            [compojure.handler :refer [site api]]
+            [org.httpkit.server :refer [with-channel on-close on-receive run-server send!]]
+            [ring.util.response :as resp]
+            [cemerick.friend :as friend]
+            [cemerick.friend.workflows :as workflows]
+            [cemerick.friend.credentials :as creds]
+            [clojure.core.async :refer [timeout sub chan <!! >!! <! >! go go-loop] :as async]
+            [com.ashafa.clutch.utils :as utils]
+            [com.ashafa.clutch :refer [couch]]
+            #_[postal.core :as postal]
+            [clojure.tools.logging :refer [info warn error]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(def server-state (atom nil))
+
+
+(deftemplate static-page
+  (io/resource "public/index.html")
+  []
+  [:#bootstrap-css] (set-attr "href" "static/bootstrap/bootstrap-3.1.1-dist/css/bootstrap.min.css")
+  [:#bootstrap-theme-css] (set-attr "href" "static/bootstrap/bootstrap-3.1.1-dist/css/bootstrap-theme.min.css")
+  [:#react-js] (set-attr "src" "static/react/react-0.9.0.min.js")
+  [:#jquery-js] (set-attr "src" "static/jquery/jquery-1.11.0.min.js")
+  [:#bootstrap-js] (set-attr "src" "static/bootstrap/bootstrap-3.1.1-dist/js/bootstrap.min.js")
+  [:#js-files] (substitute (html [:script {:src "js/main.js" :type "text/javascript"}])))
+
+
+(defn create-store
+  "Creates a konserve store"
+  [state]
+  (swap!
+   state
+   (fn [old new] (assoc-in old [:store] new))
+   #_(<!! (new-mem-store))
+   (<!! (new-couch-store
+             (couch (utils/url (:couchdb-url @state) "lesezeichen"))
+             (:tag-table @state))))
+  state)
+
+(defn- cred-fn [creds]
+  (creds/bcrypt-credential-fn {"eve@polyc0l0r.net" {:username "eve@polyc0l0r.net"
+                                                    :password (creds/hash-bcrypt "lisp")
+                                                    :roles #{::user}}} creds))
+(defn- auth-fn [users]
+  (go (println "AUTH-REQUIRED: " users)
+    {}))
+
+(defn create-peer
+  "Creates geschichte server peer"
+  [state]
+  (let [{:keys [proto host port build tag-table store trusted-hosts]} @state]
+    (swap! state
+           (fn [old new] (assoc-in old [:peer] new))
+           (server-peer (create-http-kit-handler!
+                         (str (if (= proto "https") "wss" "ws") ;; should always be wss with auth
+                              "://" host
+                              (when (= :dev build)
+                                (str ":" port))
+                              "/geschichte/ws")
+                         tag-table)
+                        store
+                        (partial auth store auth-fn cred-fn (atom (or (:trusted-hosts @state) #{}))))))
+  state)
+
+(defroutes handler
+  (resources "/")
+
+  (GET "/geschichte/ws" [] (-> @server-state :peer deref :volatile :handler))
+
+  (GET "/*" [] (if (= (:build @server-state) :prod)
+                 (static-page)
+                 (io/resource "public/index.html"))))
+
+
+(defn read-config [state path]
+  (let [config (-> path slurp read-string
+                   (update-in [:couchdb-url] eval) ;; maybe something better but I don't want to deal withj system vars in here
+                   (assoc :tag-table
+                     (atom {'datascript.Datom
+                            (fn [val] (info "DATASCRIPT-DATOM:" val)
+                              (konserve.literals.TaggedLiteral. 'datascript.Datom val))})))]
+    (swap! state merge config))
+  state)
+
+
+(defn init
+  "Read in config file, create sync store and peer"
+  [state path]
+  (-> state
+      (read-config path)
+      create-store
+      create-peer))
+
+
+(defn start-server [port]
+  (do
+    (info (str "Starting server @ port " port))
+    (run-server (site #'handler) {:port port :join? false})))
+
+
+(defn -main [& args]
+  (init server-state (first args))
+  (info (clojure.pprint/pprint @server-state))
+  (start-server (:port @server-state)))
+
+
+(comment
+
+  (init server-state "resources/server-config.edn")
+
+  (def server (start-server (:port @server-state)))
+
+  (server)
+
+)
