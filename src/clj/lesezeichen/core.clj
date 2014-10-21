01704e07f0f1895ac268695d6e88fefcaec11bf6
kordano <konrad.kuehne@rocketmail.com>
2014-10-21 15:04:58 +0200

    revert to non-static dev

--- src/clj/lesezeichen/core.clj
+++ src/clj/lesezeichen/core.clj
@@ -20,7 +20,7 @@
   [:#react-js] (set-attr "src" "static/react/react-0.9.0.min.js")
   [:#jquery-js] (set-attr "src" "static/jquery/jquery-1.11.0.min.js")
   [:#bootstrap-js] (set-attr "src" "static/bootstrap/bootstrap-3.1.1-dist/js/bootstrap.min.js")
-  ;;[:#js-files] (substitute (html [:script {:src "js/main.js" :type "text/javascript"}]))
+  [:#js-files] (substitute (html [:script {:src "js/main.js" :type "text/javascript"}]))
   )
 
 
@@ -71,8 +71,7 @@
 (defroutes handler
   (resources "/")
   (GET "/bookmark/ws" [] bookmark-handler)
-  (GET "/*" [] (static-page)
-       #_(if (= (:build @server-state) :prod)
+  (GET "/*" [] (if (= (:build @server-state) :prod)
                  (static-page)
                  (io/resource "public/index.html"))))
 
