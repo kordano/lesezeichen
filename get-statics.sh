69f31ebf5ced9a9a067cd88a6afd5065a579a999
kordano <konrad.kuehne@rocketmail.com>
2015-01-10 15:06:28 +0100

    remove write-protected files

--- get-statics.sh
+++ get-statics.sh
@@ -11,14 +11,14 @@ mkdir bootstrap-material
 wget https://github.com/twbs/bootstrap/releases/download/v3.2.0/bootstrap-3.2.0-dist.zip -O bootstrap.zip
 unzip bootstrap.zip -d bootstrap-3.2.0
 mv bootstrap-3.2.0/bootstrap-3.2.0-dist/* bootstrap-3.2.0
-rm -r bootstrap-3.2.0/bootstrap-3.2.0-dist
+rm -rf bootstrap-3.2.0/bootstrap-3.2.0-dist
 rm bootstrap.zip
 
 
 # bootstrap material
 git clone https://github.com/FezVrasta/bootstrap-material-design.git
 mv bootstrap-material-design/dist/* bootstrap-material
-rm -r bootstrap-material-design
+rm -rf bootstrap-material-design
 
 # react
 wget https://fb.me/react-0.12.2.js
