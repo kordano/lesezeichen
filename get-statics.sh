7342e872ee76cb0c2858b589985216f30d166609
kordano <konrad.kuehne@rocketmail.com>
2015-01-10 14:42:27 +0100

    adjust statics fetcher

--- get-statics.sh
+++ get-statics.sh
@@ -4,14 +4,21 @@
 cd resources/public
 mkdir static
 cd static
-mkdir bootstrap-3.3.1
+mkdir bootstrap-3.2.0
+mkdir bootstrap-material
 
 # bootstrap
-wget https://github.com/twbs/bootstrap/releases/download/v3.3.1/bootstrap-3.3.1-dist.zip -O bootstrap.zip
-unzip bootstrap.zip -d bootstrap-3.3.1
-mv bootstrap-3.3.1/dist/* bootstrap-3.3.1
-rm -r bootstrap-3.3.1/dist
+wget https://github.com/twbs/bootstrap/releases/download/v3.2.0/bootstrap-3.2.0-dist.zip -O bootstrap.zip
+unzip bootstrap.zip -d bootstrap-3.2.0
+mv bootstrap-3.2.0/dist/* bootstrap-3.2.0
+rm -r bootstrap-3.2.0/dist
 rm bootstrap.zip
+
+
+# bootstrap material
+git clone https://github.com/FezVrasta/bootstrap-material-design.git
+mv bootstrap-material-design/dist/* bootstrap-material
+rm -r bootstrap-material-design
 
 # react
 wget https://fb.me/react-0.12.2.js
